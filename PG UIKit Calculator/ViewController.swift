6f69b6792d44c5036b537faf9539d20894c52dbc
Pāvels Garklāvs <pavels.garklavs@accenture.com>
2022-02-08 11:26:11 +0200

    Initial Commit

---
+++ PG UIKit Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PG UIKit Calculator
+//
+//  Created by pavels.garklavs on 08/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
